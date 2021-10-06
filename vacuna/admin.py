24e47623e3dd29c4216762ba73a8f7832668d30c
Daniel-10Melgarejo <63325639+Daniel-10Melgarejo@users.noreply.github.com>
2021-10-05 22:26:15 -0300

    Vacunate01

---
+++ vacuna/admin.py
@@ -0,0 +1,15 @@
+from django.contrib import admin
+
+from vacuna.models import Modalidad_Vacunatorio, Puesto, Puesto_Tipo_Vacunatorio, Sede, Tipo_de_Enfermedad, Usuario, Usuario_Vacuna, Vacuna, Vacuna_Puesto
+
+# Register your models here.
+
+admin.site.register(Sede)
+admin.site.register(Puesto)
+admin.site.register(Modalidad_Vacunatorio)
+admin.site.register(Puesto_Tipo_Vacunatorio)
+admin.site.register(Vacuna_Puesto)
+admin.site.register(Tipo_de_Enfermedad)
+admin.site.register(Vacuna)
+admin.site.register(Usuario)
+admin.site.register(Usuario_Vacuna)
---
+++ vacuna/models.py
@@ -0,0 +1,86 @@
+from django.db import models
+from django.db.models.deletion import CASCADE
+from django.db.models.expressions import Case
+
+# Create your models here.
+
+
+class Sede(models.Model):
+    nombre = models.CharField(max_length=200)
+    habilitado = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.nombre
+
+
+class Puesto(models.Model):
+    nombre = models.CharField(max_length=200)
+    habilitado = models.BooleanField(default=False)
+    sede = models.ForeignKey(Sede, on_delete=CASCADE)
+
+    def __str__(self):
+        return self.nombre
+
+
+class Modalidad_Vacunatorio(models.Model):
+    nombre = models.CharField(max_length=100)
+    descripcion = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.nombre
+
+
+
+class Puesto_Tipo_Vacunatorio(models.Model):
+   puesto = models.ForeignKey(Puesto, on_delete=CASCADE)
+   habilitado = models.BooleanField(default=False)
+   modalidadvacunatorio = models.ForeignKey(Modalidad_Vacunatorio, on_delete=CASCADE)
+
+#ver que columnas mas se le puede agregar a esta table de Vacuna_Puesto
+class Vacuna_Puesto(models.Model):
+    puesto = models.ForeignKey(Puesto, on_delete=CASCADE)
+
+
+class Tipo_de_Enfermedad(models.Model):
+    nombre = models.CharField(max_length=150)
+
+
+class Vacuna(models.Model):
+    nombre = models.CharField(max_length=100)
+    tipoenfermedad = models.ForeignKey(Tipo_de_Enfermedad, on_delete=CASCADE)
+    cantidaddosis = models.IntegerField(int)
+    rangoetarioinicio = models.IntegerField(int)
+    rangoetariofin = models.IntegerField(int)
+    periodoentredosis =  models.IntegerField(int)
+
+
+class Usuario(models.Model):
+    nombre = models.CharField(max_length=150)
+    apellido = models.CharField(max_length=150)
+    edad = models.IntegerField(int)
+    cedula = models.CharField(max_length=100)
+    ciudad = models.CharField(max_length=150)
+    password = models.CharField(max_length=150)
+
+
+class Usuario_Vacuna(models.Model):
+    puestoname = models.ForeignKey(Puesto, on_delete=CASCADE)
+    vacunaname = models.ForeignKey(Vacuna, on_delete=CASCADE)
+    usuarioname = models.ForeignKey(Usuario, on_delete=CASCADE)
+    cantidaddedosis = models.IntegerField(int)
+    periodoentredosisdias = models.IntegerField(int)
+
+
+
+    
+
+
+
+
+
+
+
+
+
+
+
