bf48397cda41d31d41d4a0c1684ac2a7dc7c9095
Daniel-10Melgarejo <63325639+Daniel-10Melgarejo@users.noreply.github.com>
2021-10-17 21:29:24 -0300

    tutorial 5

--- polls/models.py
+++ polls/models.py
@@ -10,6 +10,11 @@ class Question(models.Model):
     def __str__(self):
     	return self.question_text
 
+    def was_published_recently(self):
+        now = timezone.now()
+        return now - datetime.timedelta(days=1) <= self.pub_date <= now
+
+
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
@@ -17,4 +22,5 @@ class Choice(models.Model):
     def __str__(self):
         return self.choice_text
     def was_published_recently(self):
-        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+        now = timezone.now()
+        return now - datetime.timedelta(days=1) <= self.pub_date <= now
